58ec35443e21552b106dff0ab16ed83e3fff7b1f
Devout123 <1919574160@qq.com>
2024-07-09 15:16:05 +0800

    Add files via upload

---
+++ USER/app.js
@@ -0,0 +1,28 @@
+// app.js
+App({
+  onLaunch() {
+  },
+  globalData: {
+    userInfo: null,
+    //IAM的网址
+    iamEndpoint: "iam.cn-north-4.myhuaweicloud.com",   
+    // 账号名
+    userName: "yuejiaping123",
+    // IAM用户名
+    iamUserName: "yuejiaping1",
+    // IAM密码
+    iamUserPassword: "qaz123",
+    // 所属项目名
+    productName: "cn-north-4",
+    // IoTDA的网址
+    iotDAEndpoint: "00cb2991d1.st1.iotda-app.cn-north-4.myhuaweicloud.com",
+    // 产品ID
+    project_id: "6683c333a559ef6226665126",
+    // 项目ID
+    product_id: "a9dc070aa62043ca98308261d720fdc1",
+    // 设备ID
+    device_id: "6683c333a559ef6226665126_hi3861",
+
+    color: 'rgb(0,0,0)'
+  }
+})
